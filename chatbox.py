9420673a25ca35da614740769651cc7e762a2106
Nakato-Musana-Zaina <nakatozainamusana@gmail.com>
2024-05-17 17:35:50 +0300

    made improvements

--- chatbox.py
+++ chatbox.py
@@ -4,12 +4,12 @@ import json
 from difflib import get_close_matches
 
 
-def load_knowledge_base(file_path: str)-> dict:
+def load_content(file_path: str)-> dict:
     with open(file_path, 'r') as file:
             data: dict = json.load(file)
     return data
 
-def save_knowledge_base(file_path:str, data: dict):
+def save_content(file_path:str, data: dict):
     with open(file_path, 'w') as file:
          json.dump(data,file,indent=2)
 
@@ -17,8 +17,8 @@ def find_best_match(user_question: str, questions:list[str])-> str | None:
      matches: list = get_close_matches(user_question,questions, n=1,cutoff=0.7)
      return matches[0] if matches else None
 
-def get_answer_for_question(question:str, knowledge_base:dict)-> str | None:
-     for q in knowledge_base["Questions"]:
+def get_answer_for_question(question:str, content:dict)-> str | None:
+     for q in content["Questions"]:
           if q["question"] == question:
                return q["answer"]
           
@@ -26,7 +26,7 @@ def get_answer_for_question(question:str, knowledge_base:dict)-> str | None:
 
 
 def chat_bot():
-     knowledge_base: dict= load_knowledge_base('knowledge_base.json')
+     content: dict= load_content('content.json')
 
      while True:
           user_input: str = input('You: ')
@@ -34,18 +34,18 @@ def chat_bot():
           if user_input.lower() == 'quit':
                break
           
-          best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["Questions"]])
+          best_match: str | None = find_best_match(user_input, [q["question"] for q in content["Questions"]])
 
           if best_match:
-               answer: str = get_answer_for_question(best_match, knowledge_base)
+               answer: str = get_answer_for_question(best_match, content)
                print(f"Bot: {answer}")
           else: print('Bot: I do not know the answer. Can you teach me? ')
           new_answer: str = input('Write your answer or "skip" to skip: ')
 
 
           if new_answer.lower()!= 'skip':
-               knowledge_base["Questions"].append({"question":user_input, "answer":new_answer})
-               save_knowledge_base("knowledge_base.json", knowledge_base)
+               content["Questions"].append({"question":user_input, "answer":new_answer})
+               save_content("content.json", content)
                print("Bot:Thank you! I have Learned a new response")
 
 
@@ -54,8 +54,13 @@ if __name__ == '__main__':
       chat_bot()
 
               
+# def integral(n):
+#      my_dict= { }
+#      for number in range(1,n+1):
+#           my_dict[number] = number*number
+#      return my_dict
 
-
+# print(integral(8))
           
 
           
---
+++ te.js
@@ -0,0 +1,12 @@
+function word(words){
+    all = 0
+    for(i in words){
+        i = words.length-1
+        while(i >=0){
+            return all += i
+        }
+        i--
+    }
+  
+}
+console.log(word("mangos"))
